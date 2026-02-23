d8acad6dc407f527c2407772ac6d5a2404bb69bb
Raimonds-1964 <mediatelecom@inbox.lv>
2026-02-23 20:59:44 +0200

    Fix manager campaigns entry: use CampaignsClient (no Client)

--- aicampaign-en/app/(agency)/agency/manager/campaigns/page.tsx
+++ aicampaign-en/app/(agency)/agency/manager/campaigns/page.tsx
@@ -2,8 +2,8 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const fetchCache = "force-no-store";
 
-import Client from "./Client";
+import CampaignsClient from "./CampaignsClient";
 
 export default function Page() {
-  return <Client />;
+  return <CampaignsClient />;
 }
